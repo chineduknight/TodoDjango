889a54ed51d63294f4c9084859247f755d851553
Chinedu Oguejiofor <chineduknight@gmail.com>
2024-01-11 20:15:28 +0100

    updated the allowed host env var

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 AUTH_USER_MODEL="authentication.User"
 # Application definition
